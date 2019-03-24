faf7f9711de576728bc54048046baf70c8877eef
Emi Katsuta <emi.katsuta@gmail.com>
2019-03-24 19:02:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function takeANumber(currentLine, name) {
-  currentLine.push(name);
-  return `Welcome, ${name}. You are number ${currentLine.length} in line.`
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
+function nowServing(katzDeliLine){
+  if (!katzDeliLine.length){
+    return "There is nobody waiting to be served!"
+  } else {
+    let firstPerson = katzDeliLine[0];
+    katzDeliLine.shift();
+    return firstPerson;
+  }
+}
